b3758fafc6bfa2eeaa0553d6def2c0fd3c15af3f
Cameron MacKay <40600212+CMackay7@users.noreply.github.com>
2019-11-26 16:33:29 +0000

    Update PluralityVote.py

--- Python Backend/PluralityVote.py
+++ Python Backend/PluralityVote.py
@@ -1,5 +1,20 @@
 class PluralityVote:
 
-    def __init__(vote):
-        vote = vote
+    def __init__(self, vote):
+        self.vote = vote
+
+    def BestFPTP(self):
+
+
+
+    def  calc_winner(self, votebreakdown):
+        winner = ""
+        winning_votes = 0
+
+        for candidate in votebreakdown:
+            if votebreakdown[candidate] > winning_votes:
+                winner = candidate
+                winning_votes = votebreakdown[candidate]
+
+        return winner
 
