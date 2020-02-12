0f7fbd84833778d64f7648e0acfedc15e309593c
Cameron MacKay <40600212+CMackay7@users.noreply.github.com>
2020-02-12 15:23:45 +0000

    updated score vote runner

--- Python Backend/ScoreVoteRunner.py
+++ Python Backend/ScoreVoteRunner.py
@@ -6,43 +6,43 @@ class ScoreVoteRunner:
         self.can_add_remove = addremove_boolean
 
     def run_election(self):
-        return_dict = {}
+        return_dict = []
         candidate_to_win = self.score_vote.candidateToWin
 
         sumvote = self.run_sum_vote()
-        if sumvote[0] == candidate_to_win:
-            return_dict.update(sumvote[1])
+        if sumvote == candidate_to_win:
+            return_dict.append("SMV")
 
         meanvote = self.run_mean_vote()
-        if meanvote[0] == candidate_to_win:
-            return_dict.update(meanvote[1])
+        if meanvote == candidate_to_win:
+            return_dict.append("MNV")
 
         starvote = self.run_star_vote()
-        if starvote[0] == candidate_to_win:
-            return_dict.update(starvote[1])
+        if starvote == candidate_to_win:
+            return_dict.append("STR")
 
         return return_dict
 
     def run_star_vote(self):
-        updates = {"nil": "nil"}
-        json = {"STR":updates}
+        #updates = {"nil": "nil"}
+        #json = {"STR":updates}
         breakdown = self.score_vote.star_vote()
         winner = max(breakdown, key=breakdown.get)
-        return winner, json
+        return winner
 
     def run_mean_vote(self):
-        updates = {"nil": "nil"}
-        json = {"MN": updates}
+        #updates = {"nil": "nil"}
+        #json = {"MN": updates}
         breakdown = self.score_vote.mean_vote()
         winner = max(breakdown, key=breakdown.get)
-        return winner, json
+        return winner
 
     def run_sum_vote(self):
-        updates = {"nil": "nil"}
-        json = {"SM": updates}
+        #updates = {"nil": "nil"}
+        #json = {"SM": updates}
         breakdown = self.score_vote.sum_vote()
         winner = max(breakdown, key=breakdown.get)
-        return winner, json
+        return winner
 
 
 
